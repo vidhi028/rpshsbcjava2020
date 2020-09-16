bffad1b7c254af40eab565f7ff05c1e894e9105f
eswaribala <Parameswaribala@gmail.com>
2020-09-16 16:43:18 +0530

    Add files via upload

---
+++ TraineesApp.java
@@ -0,0 +1,43 @@
+package com.hsbc.banking.utility;
+
+import java.util.Random;
+
+import com.hsbc.banking.models.Trainee;
+
+public class TraineesApp {
+	
+	//Trainee Array created
+	private static Trainee[] traineeList=new Trainee[5];	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//add 5 trainees		
+		createTrainees();
+		getTrainees();		
+		
+	}	
+	
+	private static void createTrainees()
+	{
+					
+				//add trainee object to array		
+				for(int i=0;i<traineeList.length;i++)
+				{
+					traineeList[i]=new Trainee();			
+					traineeList[i].setUserName("user"+new Random().nextInt(10000));
+					traineeList[i].setPassword("Pwd"+new Random().nextInt(100000)+"!");			
+				}
+	}
+	
+	
+	
+	private static void getTrainees()
+	{
+		
+		//retrieve all the user names and password		
+				for(Trainee trainee:traineeList)
+							System.out.println("UserName="+trainee.getUserName()+","
+							+ "Password="+trainee.getPassword());
+	}
+
+}
